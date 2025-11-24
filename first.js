14b2b5ca90b871699dc8332667d3bf10d15c2d87
AlexandreCourtin <acourtin@student.42.fr>
2025-11-24 16:08:27 +0100

    music visualisation

--- first.js
+++ first.js
@@ -1,13 +1,17 @@
-$: s("<[hh hh] cp [- hh cp hh] cp>*4")
-  .lpf("<500 700 900 1100 1300 1500 1500 1500 1500 1500>/4")
-  .mask("<1 1 1 0 0>/8")
-  .theme("default")
+stack(
+  s("<[hh hh] cp [- hh cp hh] cp>*4")
+    .lpf("<500 700 900 1100 1300 1500 1500 1500 1500 1500>/4")
+    .mask("<1 1 1 0 0>/8")
+    .theme("default")._scope()
 
-$: n("<<-10>*4 <0>*4 <-10>*16 <0>*16>/2").scale("g:minor").trans(-12).s("supersaw")
-  .mask("<1 1 1 1 0>/8")
+  ,n("<<-10>*4 <0>*4 <-10>*16 <0>*16>/2").scale("g:minor").trans(-12).s("supersaw")
+    .mask("<1 1 1 1 0>/8")
 
-$: n("<-*4 [1 2 3 4] -*4 [1 2 3 4]*2>").scale("c:minor").s("gm_acoustic_bass")
-  .mask("<0 1 1 1 0>/8")
+  ,stack(
+    n("<-*4 [1 2 3 4] -*4 [1 2 3 4]*2>").scale("c:minor").s("gm_acoustic_bass")
+      .mask("<0 1 1 1 0>/8")
 
-$: n("<[4 3 2 1]*2 -*4 [4 3 2 1]*2 -*4>").scale("c:minor").s("gm_acoustic_bass")
-  .mask("<0 0 1 1 0>/8")
+    ,n("<[4 3 2 1]*2 -*4 [4 3 2 1]*2 -*4>").scale("c:minor").s("gm_acoustic_bass")
+      .mask("<0 0 1 1 0>/8")
+  )._pianoroll()
+).cpm(30)
