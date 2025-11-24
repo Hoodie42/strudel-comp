521dc10ce1f4fc2f75a3ff98e00c1da6ebbd695e
AlexandreCourtin <acourtin@student.42.fr>
2025-11-24 17:50:56 +0100

    added smb

---
+++ smb.js
@@ -0,0 +1,51 @@
+/*
+  @title Super Mario Bros
+  @by Hoodie
+  @version 1
+*/
+
+stack(
+  n(`<
+    e3 e3 - e3 - c3 e3 - g3 - - - g2 - - -
+
+    c3 - - g2 - - e2 - -
+    a2 - b2 - bb2 a2 - g2 -
+    e3 g3 a3 - f3 g3 - e3 - c3 - d3 b2 -
+
+    c3 - - g2 - - e2 - -
+    a2 - b2 - bb2 a2 - g2 -
+    e3 g3 a3 - f3 g3 - e3 - c3 - d3 b2 - - -
+
+    g3 f3 f3 d3 - e3 -
+    g2 a2 c3 - a2 c2 d3 - -
+    g3 f3 f3 d3 - e3 -
+    c4 - c4 c4 - - - -
+
+    g3 f3 f3 d3 - e3 -
+    g2 a2 c3 - a2 c2 d3 - -
+    d#3 - - d3 - - c3 - - - - -
+
+    c3 c3 - c3 - c3 d3 - e3 c3 - a2 g2 - - -
+    c3 c3 - c3 - c3 d3 - e3 - - - - -
+
+    c3 c3 - c3 - c3 d3 - e3 c3 - a2 g2 - - -
+    e3 e3 - e3 - c3 e3 - g3 - - - g2 - - -
+
+    c3 - - g2 - - e2 - -
+    a2 - b2 - bb2 a2 - g2 -
+    e3 g3 a3 - f3 g3 - e3 - c3 - d3 b2 -
+
+    c3 - - g2 - - e2 - -
+    a2 - b2 - bb2 a2 - g2 -
+    e3 g3 a3 - f3 g3 - e3 - c3 - d3 b2 - - -
+
+    e3 c3 - g2 - - g2 - a2 f3 - f3 a2 - - -
+    b2 a3 a3 a3 g3 f3 - e3 c3 - a2 g2 - - -
+
+    e3 c3 - g2 - - g2 - a2 f3 - f3 a2 - - -
+    b2 f3 f3 f3 e3 d3 c3 g2 - e2 c2 - - - - -
+  >*10`)
+    .s("gm_acoustic_bass")
+    .scale("c:major")
+    .theme("default")
+).cpm(30)
